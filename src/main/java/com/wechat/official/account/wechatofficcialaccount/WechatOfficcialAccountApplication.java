3ef6798769b4f3883b34204264cbd54a9a0d476c
xiebing <18671989765@163.com>
2020-09-11 10:06:28 +0800

    wechat

---
+++ src/main/java/com/wechat/official/account/wechatofficcialaccount/WechatOfficcialAccountApplication.java
@@ -0,0 +1,13 @@
+package com.wechat.official.account.wechatofficcialaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatOfficcialAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatOfficcialAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wechat/official/account/wechatofficcialaccount/WechatOfficcialAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wechat.official.account.wechatofficcialaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatOfficcialAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
